e63ca8525f75333a59d299f1a990bf3d4a4712fa
Ankit Kumar <88437909+daniel-ankit@users.noreply.github.com>
2021-11-22 15:00:06 +0530

    Add files via upload

---
+++ COOK135B - ALLDIV3.cpp
@@ -0,0 +1,79 @@
+/*
+┌─────────────────────────────────────────────────────────────────────┐
+│                                                                     │
+└─────────────────────────────────────────────────────────────────────┘
+*/
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+
+#define TEST int T; cin >> T; while (T--)
+#define LL long long 
+#define PB push_back
+#define PP pop_back
+#define MP make_pair
+#define MOD(n) n%1000000007
+#define VII vector <pair<int, int>>
+#define VI vector<int>
+#define VS vector<string>
+#define VC vector<char>
+#define VB vector<bool>
+#define FF first
+#define SS second
+#define LONGINT << fixed << setprecision(0)
+#define FOR(i, n) for(long long i=0; i<n; i++)
+#define Radhe ios_base::sync_with_stdio(false);
+#define Krishna cin.tie(NULL); cout.tie(NULL);
+#define VIN(X, N) for(int i=0; i<N; i++){int V; cin >> V; H.push_back(V);};
+#define VOUT(x,X) for(auto x:X) cout << x << " ";
+#define SORT(V) sort(V.begin(), V.end())
+#define REVERSE(V) reverse(V.begin(), V.end())
+#define ADD(V) accumulate(V.begin(), V.end(), 0)
+#define PI 3.1415926535897932384626433832795
+#define endl '\n'
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+long long lcm(int a, int b) {return (a / __gcd(a, b)) * b;}
+bool sortbysec(const pair<int,int> &a, const pair<int,int> &b){return (a.second < b.second);}
+int maxfreqof(vector<int>& nums) { int count=0, ans=0; for(int n:nums) { if(count==0) ans = n; (n==ans) ? (count++) : (count--); } return ans; }
+int MEX(set <int> V){ set<int> :: iterator j; int i=0; for (j=V.begin(); j!=V.end(); j++, i++) if(*j!=i) return i; return *(V.end())+1; }
+int maxfreq(vector<int> V) { int C=1, MAX=0; SORT(V); int pivot = V[0]; for(int i=1; i<V.size(); i++) { if(V[i]!=pivot) { pivot = V[i]; C=0; } C++; MAX = max(MAX, C); } return MAX; }
+
+int32_t main()
+{
+    Radhe Krishna
+    TEST
+    {
+        LL N;
+        cin >> N;
+        vector<LL> V (N);
+        for (LL i = 0; i < N; i++)
+            cin >> V[i];
+        vector<LL> one, two, zero;
+        for (LL i = 0; i < N; i++)
+        {
+            if (V[i] % 3 == 0) zero.PB(V[i]);
+            else if (V[i] % 3 == 1) one.PB(V[i]);
+            else two.PB(V[i]);
+        }
+        if (one.size() == two.size() && one.size() != 0)
+            cout << one.size() << endl;
+        else
+        {
+            if (zero.size() == N) cout << 0 << endl;
+            else
+            {
+                if (one.size() != two.size())
+                {
+                    int a = one.size(), b = two.size();
+                    if (abs(a - b) % 3 == 0) cout << min(a, b) + 2*abs(a-b)/3 << endl;
+                    else cout << -1 << endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
